9b9795301f2090d285c3b320fa9b389fab61189e
James <a9261020@gmail.com>
2020-04-30 18:01:21 +0800

    update

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,6 +1,44 @@
 <template>
   <div class="layout">
-    12345
+    <!-- nav -->
+    <nav>
+      <ul>
+        <li><a href="">KKBOX練習</a></li>
+        <li>
+          <input type="text" />
+        </li>
+        <li>
+          <button>Search</button>
+        </li>
+      </ul>
+    </nav>
+
+    <!-- footer -->
+    <footer>
+      <div class="container">
+        <div class="col-3 tech">
+          <h3 class="col-3-title">技術運用</h3>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+        <div class="col-3 source">
+          <h3 class="col-3-title">參考資源</h3>
+          <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+          </ul>
+        </div>
+
+        <div class="col-6">
+          <input type="text" />
+          <button>Search</button>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
