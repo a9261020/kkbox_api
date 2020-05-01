7d0df9f38c3e9a9392ddada8d60ffe502a8e9edd
James <a9261020@gmail.com>
2020-05-01 20:19:00 +0800

    add SongList.vue

---
+++ src/components/SongList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="song-list">
+    <ul>
+      <li class="song-list-item" v-for="item in 5" :key="item">
+        <div class="item-rank ds-inline-block">
+          {{ item }}
+        </div>
+        <img class="item-img" src="http://fakeimg.pl/200x200" alt="" />
+        <div class="item-info">
+          <h5 class="item-songTitle">Change</h5>
+          <span class="item-singer">瘦子</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/views/Chart.vue
+++ src/views/Chart.vue
@@ -1,10 +1,22 @@
 <template>
   <div class="chart">
-    <div class="top5-chart">
+    <div class="chart-list">
       <h2 class="chart-title">綜合新歌 TOP 5</h2>
-      <router-link to="/1234" class="chart-top5-btn">
+      <router-link to="/1234" class="chart-list-btn">
         <a href="">完整榜單</a>
       </router-link>
     </div>
+    <SongList />
+    <SongList />
   </div>
 </template>
+
+<script>
+import SongList from "@/components/SongList";
+
+export default {
+  components: {
+    SongList,
+  },
+};
+</script>
